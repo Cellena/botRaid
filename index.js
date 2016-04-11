a9789d44d3d6a75399dc9a0228817cba449d8f1e
Igor.Suhoverhov <igor.suhoverhov@ocrv.ru>
2016-04-11 11:42:49 +0300

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '211353716:AAEUu013rJ5stdYkMtDtHl1vfmiyHfvHlPM';
 var bot = new TelegramBot(token, {polling: true});
 
-var User    = require('./libs/mongoose').User;
+//var User    = require('./libs/mongoose').User;
 
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
@@ -13,57 +13,58 @@ bot.on('message', function (msg) {
 
     if (msg.text.indexOf("friend") == 1){
       var friendUserId =  msg.text.split(' ')[1];
-      User.findOne({id: friendUserId}, function (err, user) {
-        if (!user) {
-          bot.sendMessage(chatId, "К сожалению, пользователь ещё не обращался к боту. Необходимо, чтобы он добавился в систему.");
-        }
-        else {
-          User.findOne({id: chatId}, function (err, user) {
-            if (!user) {
-
-            }
-            else {
-              user.current_friend_id = friendUserId;
-              user.save(function (err, newUser) {
-                if (err) {
-                  console.log("Something goes wrong with user " + newUser.id);
-                } else {
-                  console.log("Data is updated for id" + newUser.id)
-                }
-              });
-            }
-
-          });
-        }
-      });
+      bot.sendMessage(chatId, friendUserId);
+      //User.findOne({id: friendUserId}, function (err, user) {
+      //  if (!user) {
+      //    bot.sendMessage(chatId, "К сожалению, пользователь ещё не обращался к боту. Необходимо, чтобы он добавился в систему.");
+      //  }
+      //  else {
+      //    User.findOne({id: chatId}, function (err, user) {
+      //      if (!user) {
+      //
+      //      }
+      //      else {
+      //        user.current_friend_id = friendUserId;
+      //        user.save(function (err, newUser) {
+      //          if (err) {
+      //            console.log("Something goes wrong with user " + newUser.id);
+      //          } else {
+      //            console.log("Data is updated for id" + newUser.id)
+      //          }
+      //        });
+      //      }
+      //
+      //    });
+      //  }
+      //});
     }
   else {
-
-    User.findOne({id: chatId}, function (err, user) {
-      if (!user) {
-        console.log('Not found');
-
-        bot.sendMessage(chatId, "Привет, " + msg.chat.first_name + "! Твой ID " + chatId + " Передай его собеседнику! ");
-
-        var newUser = new User({id: chatId, first_name: msg.chat.first_name});
-        newUser.save(function (err, newUser) {
-          if (err) {
-            console.log("Something goes wrong with user " + newUser.id);
-          } else {
-            console.log("Data is saved for id" + newUser.id)
-          }
-        });
-      }
-      else {
-        console.log('Found success');
-        if (user.current_friend_id != null) {
-          bot.sendMessage(user.current_friend_id, msg.text);
-        }
-        else {
-          bot.sendMessage(chatId, "Привет, " + msg.chat.first_name + "! Давно не виделись, не желаешь с кем-нибудь пообщаться?");
-        }
-      }
-    });
+      bot.sendMessage(chatId, 'Привет!');
+    //User.findOne({id: chatId}, function (err, user) {
+    //  if (!user) {
+    //    console.log('Not found');
+    //
+    //    bot.sendMessage(chatId, "Привет, " + msg.chat.first_name + "! Твой ID " + chatId + " Передай его собеседнику! ");
+    //
+    //    var newUser = new User({id: chatId, first_name: msg.chat.first_name});
+    //    newUser.save(function (err, newUser) {
+    //      if (err) {
+    //        console.log("Something goes wrong with user " + newUser.id);
+    //      } else {
+    //        console.log("Data is saved for id" + newUser.id)
+    //      }
+    //    });
+    //  }
+    //  else {
+    //    console.log('Found success');
+    //    if (user.current_friend_id != null) {
+    //      bot.sendMessage(user.current_friend_id, msg.text);
+    //    }
+    //    else {
+    //      bot.sendMessage(chatId, "Привет, " + msg.chat.first_name + "! Давно не виделись, не желаешь с кем-нибудь пообщаться?");
+    //    }
+    //  }
+    //});
   }
 
 });
